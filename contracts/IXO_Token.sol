886bc867bead355768f0522e3d67d4e3ce8fdbff
Veronica <veronica.21.coutts@gmail.com>
2020-09-28 15:53:17 +0200

    Created token

---
+++ contracts/IXO_Token.sol
@@ -0,0 +1,92 @@
+pragma solidity 0.6.12;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Pausable.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract IXO_Token is ERC20Pausable, Ownable {
+    using SafeERC20 for IERC20;
+
+    constructor(string memory _name, string memory _symbol)
+        public
+        ERC20(_name, _symbol)
+        ERC20Pausable()
+        Ownable()
+    {}
+
+    /**
+        ------------------------------------------------------------------------
+        Burnable functionality
+
+        This has been taken out of the:
+        @openzeppelin/contracts/token/ERC20/ERC20Burnable (v3.2.0)
+
+        This functionality has been removed from the Burnable contract and added
+        directly here in order to prevent an inheritance clash. 
+        For more information on this inheritance clash please see the README
+        ------------------------------------------------------------------------
+    */
+
+    /**
+     * @dev Destroys `amount` tokens from the caller.
+     *
+     * See {ERC20-_burn}.
+     */
+    function burn(uint256 amount) public virtual {
+        _burn(_msgSender(), amount);
+    }
+
+    /**
+     * @dev Destroys `amount` tokens from `account`, deducting from the caller's
+     * allowance.
+     *
+     * See {ERC20-_burn} and {ERC20-allowance}.
+     *
+     * Requirements:
+     *
+     * - the caller must have allowance for ``accounts``'s tokens of at least
+     * `amount`.
+     */
+    function burnFrom(address account, uint256 amount) public virtual {
+        uint256 decreasedAllowance = allowance(account, _msgSender()).sub(
+            amount,
+            "ERC20: burn amount exceeds allowance"
+        );
+
+        _approve(account, _msgSender(), decreasedAllowance);
+        _burn(account, amount);
+    }
+
+    /**
+        ------------------------------------------------------------------------
+        Mintable functionality
+
+        This functionality has been taken from the OpenZeppelin library v2.5.0:
+        @openzeppelin/contracts/token/ERC20/ERC20Mintable (v2.5.0)
+        
+        This was done to provide an audited mint functionality (as there is no
+        equivalent in OZ v3.x). The access control has been updated from the
+        Mintable Role to the Ownable role in order to reduce the amount of code
+        taken from the older library. 
+
+        For more information on this change please see the README
+        ------------------------------------------------------------------------
+    */
+
+    /**
+     * @dev See {ERC20-_mint}.
+     *
+     * Requirements:
+     *
+     * - the caller must have the {Ownable} role.
+     */
+    function mint(address account, uint256 amount)
+        public
+        onlyOwner()
+        returns (bool)
+    {
+        _mint(account, amount);
+        return true;
+    }
+}
